15efe8a679ef7d2e4de9e0cf5811cdfa187fa3e6
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-25 22:28:26 +0600

    added get user profile for auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -83,6 +83,20 @@ const loginUser = async (req, res) => {
 };
 
 // Get user profile
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+    try{
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.json(user);
+  }catch(error){
+      return res.status(500).json({
+        message: "Server error",
+        error: error.message,
+      });
+  }
+
+};
 
 module.exports = { registerUser, loginUser, getUserProfile };
