91eb524e27d494cf839d822a3a3f7221f2dcf875
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-27 17:22:45 +0600

    implement api path and instance

--- frontend/resume_builder/src/utils/axiosinstance.js
+++ frontend/resume_builder/src/utils/axiosinstance.js
@@ -1,2 +1,51 @@
-
 import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+//request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // handle common errors globally
+    if (error.response) {
+        if (error.response.status === 401) {
+            //redirect to login page
+            window.location.href = "/login";
+        } else if (error.response.status === 500) {
+            console.error("Server error: please try again later");
+        }
+        }else if (error.code === "ECONNABORTED") {
+            console.error("Request timed out. Please try again later");
+        }
+        return Promise.reject(error);
+        }
+
+);
+
+export default axiosInstance;
+
+
